9bd6da4358c48ec1711213038ad511ff1112b402
sxby <suixinbuyu@163.com>
2019-02-15 09:45:02 +0800

    1.先测试建立一个git本地库

--- src/CreationTypeDPDemos/Main.java
+++ src/CreationTypeDPDemos/Main.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello,world");
+        System.out.println("hello,world2");
     }
 }
